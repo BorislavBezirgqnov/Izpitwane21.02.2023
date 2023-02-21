3631b7303c88b13ac83137f479b8fa126e78fbad
BorislavBezirgqnov <bobkata504@gmail.com>
2023-02-21 09:12:41 +0200

    zad s Belezireva

---
+++ 21.02.2023/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21._02._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray;
+
+        }
+    }
+}
